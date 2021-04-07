92fe0f748911917bfa95a8dbd222d93314e52268
Gunnar <gunnarj17@ru.is>
2021-04-07 18:53:35 +0000

    added a functional navbar. Does not look exactely the same as in our prototype

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import * as firebase from 'firebase';
 import apiKeys from './config/keys';
+import { Ionicons } from '@expo/vector-icons';
+
+// stack screens
 import WelcomeScreen from './screens/WelcomeScreen';
 import SignUp from './screens/SignUp';
 import SignIn from './screens/SignIn';
 import LoadingScreen from './screens/LoadingScreen';
-import BottomTabScreen from './screens/BottomTabScreen';
 
+
+// Tab Screens
+import Info from './screens/InfoScreen';
+import Profile from './screens/ProfileScreen';
+import Parks from './screens/ParksScreen';
+import Events from './screens/EventsScreen';
 
 const Stack = createStackNavigator();
-
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   if (!firebase.apps.length) {
@@ -20,62 +29,68 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer >
       <Stack.Navigator>
         <Stack.Screen name={'Loading'} component={LoadingScreen} options={{ headerShown: false }} />
         <Stack.Screen name='Home' component={WelcomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: false }} />
         <Stack.Screen name='Sign In' component={SignIn} options={{ headerShown: false }} />
-        <Stack.Screen name={'BottomTabScreen'} component={BottomTabScreen} options={{ headerShown: false }} />
+
+        <Stack.Screen name={'Parks'} options={{ headerShown: false }}>{() => (
+          <Tab.Navigator
+            initialRouteName='Parks'
+            activeColor='#FFFFFF'
+            inactiveColor='#F2F9F4'
+            
+            barStyle={{
+              overflow: 'hidden',
+              backgroundColor: '#034B42',
+              // borderRadius: 30,
+            }}
+          >
+            <Tab.Screen name='Staðsetningar' component={Parks} options={{
+              tabBarIcon: ({ focused }) => (
+                <Ionicons
+                  name="location-sharp"
+                  size={22}
+                  color={focused ? 'white' : 'gray'}
+                />
+              )
+            }} />
+
+            <Tab.Screen name='Upplýsingar' component={Info} options={{
+              tabBarIcon: ({ focused }) => (
+                <Ionicons
+                  name="information-circle"
+                  size={23}
+                  color={focused ? 'white' : 'gray'}
+                />
+              )
+            }} />
+
+            <Tab.Screen name='Viðburðir' component={Events} options={{
+              tabBarIcon: ({ focused }) => (
+                <Ionicons
+                  name="calendar"
+                  size={22}
+                  color={focused ? 'white' : 'gray'}
+                />
+              )
+            }} />
+
+            <Tab.Screen name='Prófíll' component={Profile} options={{
+              tabBarIcon: ({ focused }) => (
+                <Ionicons
+                  name="person"
+                  size={22}
+                  color={focused ? 'white' : 'gray'}
+                />
+              )
+            }} />
+          </Tab.Navigator>)}
+        </Stack.Screen>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// // hjálpar með að fara til baka á seinasta skjá
-// import { NavigationContainer } from '@react-navigation/native';
-// // hjálpar til með að stacka screens ofaná hvort annað þegar verið er að navigate-a á milli skjáa
-// import { createStackNavigator } from '@react-navigation/stack';
-
-// import Home from './src/screens/Home';
-// import Parks from './src/screens/Parks';
-// import Login from './src/screens/Login';
-// import Register from './src/screens/Register';
-
-// import * as firebase from 'firebase';
-// import {firebaseConfig} from './src/firebase/config';
-
-// export default function App() {
-//   const Stack = createStackNavigator();
-
-//   return (
-
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen name="Forsíða" component={Home} />
-//         <Stack.Screen
-//           options={{ headerLargeTitle: true }}
-//           name="Hundasvæði" component={Parks} />
-//         <Stack.Screen name="Innskráning" component={Login} /> 
-//         <Stack.Screen name="Nýskráning" component={Register} /> 
-//       </Stack.Navigator>
-//     </NavigationContainer>
-
-//   );
-// }
