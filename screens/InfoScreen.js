84865d39cb052ca2c232b5225aebe78cdaf20214
Gunnar <34030985+gunnarj17@users.noreply.github.com>
2021-04-09 19:50:39 +0000

    working in info screen

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -1,86 +1,197 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import * as firebase from 'firebase';
-import { loggingOut } from '../API/firebaseMethods';
+// Example of Collapsible/Accordion/Expandable List View in React Native
+// https://aboutreact.com/collapsible-accordion-expandable-view/
 
+// import React in our code
+import React, { useState } from "react";
 
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
-export default function Dashboard({ navigation }) {
-  let currentUserUID = firebase.auth().currentUser.uid;
-  const [name, setName] = useState('');
+//import for the animation of Collapse and Expand
+import * as Animatable from "react-native-animatable";
 
-  useEffect(() => {
-    async function getUserInfo() {
-      let doc = await firebase
-        .firestore()
-        .collection('users')
-        .doc(currentUserUID)
-        .get();
+//import for the Accordion view
+import Accordion from "react-native-collapsible/Accordion";
 
-      if (!doc.exists) {
-        Alert.alert('Notandi finnst ekki, reyndu aftur.')
-      } else {
-        let dataObj = doc.data();
-        setName(dataObj.name)
-      }
-    }
-    getUserInfo();
-  })
+//Dummy content to show
+//You can also use dynamic data by calling web service
+const CONTENT = [
+  {
+    title: "Gott að vita...",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "Gott að vita...",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "Gott að vita...",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "Gott að vita...",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "Gott að vita...",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    title: "Gott að vita...",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+];
 
-  const handlePress = () => {
-    loggingOut();
-    navigation.replace('Home');
+const Info = () => {
+  // Default active selector
+  const [activeSections, setActiveSections] = useState([]);
+
+  // MultipleSelect is for the Multiple Expand allowed
+  // True: Expand multiple at a time
+  // False: One can be expand at a time
+  const [multipleSelect, setMultipleSelect] = useState(false);
+
+  const setSections = (sections) => {
+    //setting up a active section state
+    setActiveSections(sections.includes(undefined) ? [] : sections);
+  };
+
+  const renderHeader = (section, _, isActive) => {
+    //Accordion Header view
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor"
+      >
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  };
+
+  const renderContent = (section, _, isActive) => {
+    //Accordion Content view
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor"
+      >
+        <Animatable.Text
+          animation={isActive ? "bounceIn" : undefined}
+          // style={{ textAlign: "center" }}
+        >
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>Dashboard</Text>
-      <Text style={styles.text}>Hæ {name} þetta er gott að vita skjárinn!</Text>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Útskráning</Text>
-      </TouchableOpacity>
-      
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>Gott að vita</Text>
+          {/*Code for Accordion/Expandable List starts here*/}
+          <Accordion
+            activeSections={activeSections}
+            //for any default active section
+            sections={CONTENT}
+            //title and content of accordion
+            touchableComponent={TouchableOpacity}
+            //which type of touchable component you want
+            //It can be the following Touchables
+            //TouchableHighlight, TouchableNativeFeedback
+            //TouchableOpacity , TouchableWithoutFeedback
+            expandMultiple={multipleSelect}
+            //Do you want to expand mutiple at a time or single at a time
+            renderHeader={renderHeader}
+            //Header Component(View) to render
+            renderContent={renderContent}
+            //Content Component(View) to render
+            duration={400}
+            //Duration for Collapse and expand
+            onChange={setSections}
+            //setting the state of active sections
+          />
+          {/*Code for Accordion/Expandable List ends here*/}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Info;
 
 const styles = StyleSheet.create({
-  button: {
-    width: 150,
-    padding: 5,
-    backgroundColor: '#ff9999',
-    borderWidth: 2,
-    borderColor: 'white',
-    borderRadius: 15,
-    alignSelf: 'center',
-  },
-  buttonText: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
   container: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#3FC5AB',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    paddingTop: 30,
   },
-  text: {
-    textAlign: 'center',
-    fontSize: 20,
-    fontStyle: 'italic',
-    marginTop: '2%',
-    marginBottom: '10%',
-    fontWeight: 'bold',
-    color: 'black',
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "300",
+    marginBottom: 20,
   },
-  titleText: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#2E6194',
+  header: {
+    backgroundColor: "#F5FCFF",
+    padding: 10,
+  },
+  headerText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  content: {
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  active: {
+    backgroundColor: "rgba(255,255,255,1)",
+  },
+  inactive: {
+    backgroundColor: "rgba(245,252,255,1)",
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  selector: {
+    backgroundColor: "#F5FCFF",
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: "bold",
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: "500",
+    padding: 10,
+    textAlign: "center",
+  },
+  multipleToggle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 30,
+    alignItems: "center",
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
   },
 });
