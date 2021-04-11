cab50d7b4a25c85a4a0d974aadf62e8149bce8a5
mariamjoll1 <mariahrafns@gmail.com>
2021-04-11 20:05:42 +0000

    bottom sheet reddý

--- screens/ParkPreview.js
+++ screens/ParkPreview.js
@@ -1,47 +1,107 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import Animated from 'react-native-reanimated';
-import BottomSheet from 'reanimated-bottom-sheet';
+// park preview
+import * as React from "react";
+import { StyleSheet, Text, View, Button} from "react-native";
+import { Icon,} from "native-base";
+import Animated from "react-native-reanimated";
+import BottomSheet from "reanimated-bottom-sheet";
 
-const ParkPreview = (props) => {
-
-    Alert.alert(
-    "Alert Title",
-    "My Alert Msg",
-    [
-      {
-        text: "Cancel",
-        onPress: () => console.log(props),
-        style: "cancel"
-      },
-      { text: "OK", onPress: () => console.log("OK Pressed") }
-    ]
-    );
-    
-    bs = React.useRef(null);
-    fall = new Animated.Value(1);
-
-    renderInner = () => (
-        <Text>{props.name}</Text>
-    );
-
-    renderHeader = () => (
-        <View style={styles.bsHeader}>
-            <View style={styles.bsPanelHeader}>
-            <View style={styles.bsPanelHandle}></View>
-            </View>
+const ParkPreview = React.forwardRef(({ props }, ref) => {
+  console.log(props)
+  const renderInner = () => (
+    <View style={styles.panel}>
+      <View style={styles.panelTop}>
+        <Text style={styles.panelTitle}>{props && props.Name ? props.Name : "Vantar nafn"}</Text>
+      </View>
+      <View style={styles.panelBottom}>
+        <View style={styles.panelLeft}>
+          <View style={styles.iconView}>
+            <Icon style={styles.Icons} name='star'/><Icon style={styles.Icons} name='star'/><Icon style={styles.Icons} name='star'/>
+          </View>
+          <Text style={styles.leftText}> </Text>
         </View>
-    );
-    return(
-        <BottomSheet
-          ref={bs}
-          snapPoints={[330, 0]}
-          renderContent={renderInner}
-          renderHeader={renderHeader}
-          initialSnap={1}
-          callbackNode={fall}
-          enabledGestureInteraction={true}
-        />
-    );
-}
+        <View style={styles.panelRight}>
+          <Text style={styles.rightText}>Right</Text>
+        </View>
+      </View>
+    </View>
+  );
+  const renderHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle}></View>
+      </View>
+    </View>
+  );
+  const fall = new Animated.Value(1);
+
+  return (
+    <BottomSheet
+      ref={ref}
+      snapPoints={[350, 0]}
+      renderContent={renderInner}
+      renderHeader={renderHeader}
+      initialSnap={1}
+      callbackNode={fall}
+      enabledGestureInteraction={true}
+    />
+  );
+});
 export default ParkPreview;
+
+const styles = StyleSheet.create({
+  panel: {
+    padding: 20,
+    backgroundColor: "#FFFFFF",
+    paddingTop: 20,
+    backgroundColor: "white",
+  },
+  panelTop: {
+  },
+  panelTitle: {
+    fontSize: 24,
+  },
+  panelBottom: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+  panelLeft: {
+    alignSelf: 'flex-start',
+    padding: 10
+  },
+  iconView: {
+    flexDirection: 'row'
+  },
+  Icons: {
+    color: '#C4C4C4'
+  },
+  leftText: {
+    fontSize: 18
+  },
+  rightText: {
+    fontSize: 18,
+  },
+  panelRight: {
+    alignSelf: 'flex-end',
+    padding: 10
+  },
+  header: {
+    backgroundColor: "#FFFFFF",
+    shadowColor: "#969696",
+    shadowOffset: { width: 0, height: -3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  panelHeader: {
+    alignItems: "center",
+  },
+  panelHandle: {
+    width: 80,
+    height: 6,
+    borderRadius: 4,
+    backgroundColor: "#00000040",
+    marginBottom: 10,
+  },
+});
--- screens/ParksScreen.js
+++ screens/ParksScreen.js
@@ -1,35 +1,38 @@
-
 import React, { Component, useEffect, useRef } from "react";
 import MapView, { Polyline, Marker } from "react-native-maps";
-import { StyleSheet, Text, LogBox, View, Dimensions, Image, Alert } from "react-native";
-import * as Location from 'expo-location';
-import { firebaseConfig } from '../API/Firebase';
-import ParkPreview from './ParkPreview';
-// Importing for Bottom Sheet
-import Animated from 'react-native-reanimated';
-import BottomSheet from 'reanimated-bottom-sheet';
+import {
+  StyleSheet,
+  Text,
+  LogBox,
+  View,
+  Dimensions,
+  Image,
+  Alert,
+} from "react-native";
+import * as Location from "expo-location";
+import { firebaseConfig } from "../API/Firebase";
+import ParkPreview from "./ParkPreview";
 
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+import * as firebase from "firebase";
+import "firebase/firestore";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
 export default class Parks extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       data: [],
       region: null,
-    }
+      park: {},
+    };
   }
-
 
   _getLocation = async () => {
     let { status } = await Location.requestPermissionsAsync();
-    
+
     if (status !== "granted") {
       alert("Permission to access location was denied");
       return;
@@ -41,100 +44,66 @@ export default class Parks extends Component {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
       latitudeDelta: 0.0999,
-      longitudeDelta: 0.0999
-    }
-
+      longitudeDelta: 0.0999,
+    };
 
     this.setState({ region: region });
-
-  }
+  };
 
   async componentDidMount() {
     await this._getLocation();
 
-    const documentSnapshot = await firebase.firestore().collection('Parks').get();
+    const documentSnapshot = await firebase
+      .firestore()
+      .collection("Parks")
+      .get();
 
     let transformArray = [];
 
     documentSnapshot.forEach((res) => {
-
-      const { GPS } = res.data();
+      const { GPS, Name } = res.data();
 
       transformArray.push({
         ID: res.id,
+        Name: Name,
         Long: GPS.longitude,
-        Lat: GPS.latitude
+        Lat: GPS.latitude,
       });
-
     });
 
-    this.setState({ data: transformArray })
-      
+    this.setState({ data: transformArray });
   }
-  renderInner = value => (
-    <View style={styles.panel} >
-      <Text> Park name: {value} </Text>
-      {/* <Text>{value}</Text> */}
-    </View>
-  );
 
-  renderHeader = () => (
-    <View style={styles.header}>
-      <View style={styles.panelHeader}>
-        <View style={styles.panelHandle} />
-      </View>
-    </View>
-  );
-  
   bs = React.createRef();
-  fall = new Animated.Value(1);
-
-  Name = useRef();
 
   render() {
-
-    // The bottom sheet render function
-    const openBottomSheet = value => () => {
-      // <ParkPreview name={value}/>
-      this.bs.current.snapTo(0);
-      console.log(value)
-      
-    };
-
-    LogBox.ignoreLogs(['Setting a timer']);
+    LogBox.ignoreLogs(["Setting a timer"]);
 
     const { data, region } = this.state;
 
+    const openBottomSheet = (value) => {
+      this.bs.current.snapTo(0);
+      this.setState({ park: value });
+    };
+
     return (
-      <View style={styles.container}>  
-        <BottomSheet
-            ref={this.bs}
-            snapPoints={[330, 0]}
-            renderContent={this.renderInner}
-            renderHeader={this.renderHeader}
-            initialSnap={1}
-            callbackNode={this.fall}
-            enabledGestureInteraction={true}
-          />      
+      <View style={styles.container}>
+        <ParkPreview ref={this.bs} props={this.state.park} />
         <MapView
           style={styles.map}
           initialRegion={region}
           showsUserLocation={true}
         >
           {data.map((m) => {
-            Name.current = m.ID;
             return (
               <Marker
                 key={m.ID}
                 coordinate={{
                   latitude: parseFloat(m.Long),
-                  longitude: parseFloat(m.Lat)
+                  longitude: parseFloat(m.Lat),
                 }}
-                onPress={openBottomSheet(m.ID)}
-                
-                // onPress={() => this.bs.current.snapTo(0)}
+                onPress={() => openBottomSheet(m)}
               >
-                
                 <Image
                   key={m.ID}
                   style={{ width: 25, height: 25 }}
@@ -154,43 +123,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   map: {
     width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height
-  },
-  panel: {
-    padding: 20,
-    backgroundColor: '#FFFFFF',
-    paddingTop: 20,
-    backgroundColor: 'white'
-    // borderTopLeftRadius: 20,
-    // borderTopRightRadius: 20,
-    // shadowColor: '#000000',
-    // shadowOffset: {width: 0, height: 0},
-    // shadowRadius: 5,
-    // shadowOpacity: 0.4,
-  },
-  header: {
-    backgroundColor: '#FFFFFF',
-    shadowColor: '#333333',
-    shadowOffset: {width: -1, height: -3},
-    shadowRadius: 2,
-    shadowOpacity: 0.4,
-    // elevation: 5,
-    paddingTop: 20,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  panelHeader: {
-    alignItems: 'center',
-  },
-  panelHandle: {
-    width: 60,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: '#00000040',
-    marginBottom: 10,
+    height: Dimensions.get("window").height,
   },
 });
