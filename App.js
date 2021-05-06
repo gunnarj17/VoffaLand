aca25834efac8a2e0608a5d12114b9e3776b338d
mariamjoll1 <mariahrafns@gmail.com>
2021-05-06 17:15:36 +0000

    laga conflict vol 2

--- App.js
+++ App.js
@@ -44,15 +44,8 @@ export default function App() {
           <Stack.Screen name='Home' component={WelcomeScreen} options={{ headerShown: false }} />
           <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: false }} />
           <Stack.Screen name='Sign In' component={SignIn} options={{ headerShown: false }} />
-<<<<<<< HEAD
-          <Stack.Screen name='Selected Park' component={SelectedPark} options={{ headerShown: false }} />
-
-=======
           <Stack.Screen name='addDogs' component={AddDogs} options={{ headerShown: false }} />
           <Stack.Screen name='Selected Park' component={SelectedPark} options={{ headerShown: false }} />
-
-
->>>>>>> dfbb201060c87a32f0812aeb6265799f910cac61
           <Stack.Screen name={'Parks'} options={{ headerShown: false }} >{() => (
             <Tab.Navigator
                 tabBarOptions={{
